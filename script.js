800f32167af82a06b53bd534ef7f1f1ab0411b76
sneha <snehasankar24@gmail.com>
2022-07-13 15:56:54 +0530

    script file updated

--- script.js
+++ script.js
@@ -1,44 +1,47 @@
 function ValidateEmail(inputText) {
-    var mail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (inputText.value.match(mail)) {
-        alert("Valid email address!");
-        document.form1.text1.focus();
-        return true;
-    }
-    else {
-        alert("You have entered an invalid email address!");
-        document.form1.text1.focus();
-        return false;
-    }
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (inputText.value.match(mailformat)) {
+    alert("Valid email address!");
+    document.form1.text1.focus();
+    return true;
+  }
+  else {
+    alert("You have entered an invalid email address!");
+    document.form1.text1.focus();
+    return false;
+  }
 }
 
 
 document.getElementById('forms').addEventListener('submit', (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const d = Array.from(formData.entries()).reduce((memo, [key, value]) => ({
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const data = Array.from(formData.entries()).reduce((memo, [key, value]) => ({
     ...memo,
     [key]: value,
   }), {});
-    str = JSON.stringify(d);
-    console.log(str);
+  //   document.getElementById('output').innerHTML = JSON.stringify(data);
+  str = JSON.stringify(data);
+  console.log(str);
 });
 
 
-function debounce(func, timeout = 300){
-    let timerid;
-    return (...args) => {
-      clearTimeout(timerid);
-      timer = setTimeout(() => { func.apply(this, args); }, timeout);
-    };
+var button = document.getElementById("dbnc");
+const debounce = (func, delay) => {
+  let debounceTimer
+  return function () {
+    const context = this
+    const args = arguments
+    clearTimeout(debounceTimer)
+    debounceTimer
+      = setTimeout(() => func.apply(context, args), delay)
   }
-  function saveInput(){
-    console.log('Clicked');
-  }
-  const processChange = debounce(() => saveInput());
+}
+button.addEventListener('click', debounce(function () {
+  alert("This is executing after evrey 2 seconds");
+}, 2000));
 
 
-  function changecolor()
-  {
-    document.getElementById("act").style.backgroundColor = "grey";
-  }
+function changecolor() {
+  document.getElementById("act").style.backgroundColor = "grey";
+}
